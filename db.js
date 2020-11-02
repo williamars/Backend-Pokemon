652ac82770c075617d9cba7b19c56393c0ad5408
Marcelo Cesario Miguel <marcelocmiguel@outlook.com>
2020-11-02 18:08:55 -0300

    Connection no código, não consegui utilizar a variavel adicionada no heroku

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
 var mongoose = require('mongoose');
-const DB_URI= process.env.MONGODB_URI;
-const connect = () =>{
-    mongoose.connect(DB_URI,{useNewUrlParser:true});
-    const connection = mongoose.connection;
-    connection.on('error', ()=> console.error('Erro ao se conectar no mongo'));
-connection.once('open', ()=>console.log('Estamos conectados no mongo!'));
-}
-mongoose.connect(DB_URI,{useNewUrlParser:true});
-// mongoose.connect('mongodb://localhost:27017/datatest');
+const MONGO_CONNECTION= process.env.MONGO_CONNECTION;
+// const connect = () =>{
+//     mongoose.connect(DB_URI,{useNewUrlParser:true});
+//     const connection = mongoose.connection;
+//     connection.on('error', ()=> console.error('Erro ao se conectar no mongo'));
+// connection.once('open', ()=>console.log('Estamos conectados no mongo!'));
+// }
+//mongoose.connect(MONGO_CONNECTION,{useNewUrlParser:true});
+mongoose.connect('mongodb+srv://root:root123@cluster0.6l7by.mongodb.net/tecweb-db?retryWrites=true&w=majority');
 var userSchema = new mongoose.Schema({
     username: String,
     email: String
