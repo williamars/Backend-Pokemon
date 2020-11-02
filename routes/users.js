32d3ca28487e2b6d3b36cbb7b5a79ca6224815a4
Marcelo Cesario Miguel <marcelocmiguel@outlook.com>
2020-11-02 17:55:21 -0300

    Rotas index e user atualizada

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,64 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+
+/* GET Userlist page. */
+router.get('/', function(req, res) {
+  var db = require("../db");
+  var Users = db.Mongoose.model('usercollection', db.UserSchema,
+'usercollection');
+  Users.find({}).lean().exec(
+     function (e, docs) {
+       res.json(docs);
+       res.end();
+  });
+});
+
+/* GET ONE users. */
+router.get('/:id', function (req, res, next) {
+  var db = require('../db');
+  var User = db.Mongoose.model('usercollection', db.UserSchema,
+'usercollection');
+  User.find({ _id: req.params.id }).lean().exec(function (e,
+docs) {
+      res.json(docs);
+      res.end();
+  });
+});
+
+/* POST ONE users. */
+router.post('/', function (req, res, next) {
+  var db = require('../db');
+  var User = db.Mongoose.model('usercollection', db.UserSchema,
+'usercollection');
+  var newuser = new User({ username: req.body.name, email:
+req.body.email });
+  newuser.save(function (err) {
+      if (err) {
+          res.status(500).json({ error: err.message });
+          res.end();
+          return;
+      }
+      res.json(newuser);
+      res.end();
+  });
+});
+
+/* DELETE ONE user. */
+router.delete('/:id', function (req, res, next) {
+  var db = require('../db');
+  var User = db.Mongoose.model('usercollection',
+db.UserSchema, 'usercollection');
+  User.find({ _id: req.params.id }).remove(function (err) {
+      if (err) {
+          res.status(500).json({ error: err.message });
+          res.end();
+          return;
+      }
+      res.json({success: true});
+      res.end();
+  });
 });
 
 module.exports = router;
