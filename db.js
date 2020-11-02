5c5149415848ee2c13592f95ea38f386d34a1caa
Marcelo Cesario Miguel <marcelocmiguel@outlook.com>
2020-11-02 17:56:22 -0300

    forma de connect com db diferente

--- db.js
+++ db.js
@@ -1,5 +1,13 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/datatest');
+const DB_URI= process.env.MONGODB_URI;
+const connect = () =>{
+    mongoose.connect(DB_URI,{useNewUrlParser:true});
+    const connection = mongoose.connection;
+    connection.on('error', ()=> console.error('Erro ao se conectar no mongo'));
+connection.once('open', ()=>console.log('Estamos conectados no mongo!'));
+}
+mongoose.connect(DB_URI,{useNewUrlParser:true});
+// mongoose.connect('mongodb://localhost:27017/datatest');
 var userSchema = new mongoose.Schema({
     username: String,
     email: String
