71d3ba2a1e6529544baa7d0bd3c62d8a798e4374
Marcelo Cesario Miguel <marcelocmiguel@outlook.com>
2020-11-05 19:53:11 -0300

    Rota para login de usuario feita

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,5 @@
 const bodyParser = require('body-parser');
 var express = require('express');
-const { PokemonSchema } = require('../db');
 var router = express.Router();
 router.use(bodyParser.json())
 router.use(bodyParser.urlencoded({ extended: true }));  
@@ -78,20 +77,62 @@ req.body.password });
   });
 });
 
+/* LOGIN. */
+router.post('/login', function (req, res, next) {
+  var db = require('../db');
+  var User = db.Mongoose.model('tecweb-collection', db.UserSchema,
+  'tecweb-collection');
+  let usernamee = false;
+  let passwordd = false;
+  User.exists({username: req.body.username}, function(err, docs){
+    if (req.body.username == ""){
+      res.status(500).send({ message: "Username empty" });
+      return;
+    }
+    if (req.body.password == ""){
+      res.status(500).send({ message: "Password empty" });
+      return;
+    }
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+    if (docs) {
+      User.exists({password: req.body.password}, function(err, docs){
+        if (err) {
+          res.status(500).send({ message: err });
+          return;
+        }
+        if (docs) {
+          res.status(200).send({ message: "Credenciais corretas" });
+          return;
+        };
+        if (!docs){
+          res.status(400).send({ message: "Usuário ou senha incorreto" });
+          return;
+        }
+      });
+    };
+    if (!docs){
+      res.status(400).send({ message: "Usuário ou senha incorreto" });
+      return;
+    }
+  });
+  
+});
+
 // Route for creating a new Review and updating Product "review" field with it
 router.post("/:id", function(req, res) {
   var db = require('../db')
-  // var Pokemon = db.Mongoose.model('tecweb-collection', db.PokemonSchema,
-  // 'tecweb-collection');
-  // var User = db.Mongoose.model('tecweb-collection', db.UserSchema,
-  // 'tecweb-collection');
-  var Pokemon = db.Mongoose.model('tecweb-collection', db.PokemonSchema,
+  
+  var User = db.Mongoose.model('tecweb-collection', db.UserSchema,
   'tecweb-collection');
-  var newPokemon = new Pokemon({ pokemon: req.body.pokemon, type:
-    req.body.type });
+  // var Pokemon = db.Mongoose.model('tecweb-collection',db.PokemonSchema,'tecweb-collection');
+  // var newPokemon = new Pokemon({ pokemon: req.body.pokemon, type:
+  //   req.body.type });
   // var newPokemon = new Pokemon({pokemon: req.body.pokemon,type: req.body.type});
   // Create a new note and pass the req.body to the entry
-  Pokemon.create(newPokemon)
+  User.update(req.params.id,{pokemons})
     .then(function(newPokemon) {
       // If a Review was created successfully, find one Product with an `_id` equal to `req.params.id`. Update the Product to be associated with the new Review
       // { new: true } tells the query that we want it to return the updated Product -- it returns the original by default
